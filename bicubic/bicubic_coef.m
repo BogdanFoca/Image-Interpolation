a08d9ee856c8f95e5754ab73aaef4c852f84a34f
BogdanFoca <92569782+BogdanFoca@users.noreply.github.com>
2022-05-09 11:23:18 +0300

    Began bicubic

--- bicubic/bicubic_coef.m
+++ bicubic/bicubic_coef.m
@@ -4,9 +4,13 @@ function A = bicubic_coef(f, Ix, Iy, Ixy, x1, y1, x2, y2)
     % =========================================================================
 
     % TODO: Calculeaz? matricile intermediare.
-
+    first = [1, 0, 0, 0; 0, 0, 1, 0; -3, 3, -2, -1; 2, -2, 1, 1];
+    second = [f(x1, y1), f(x1, y2), Iy(x1, y1), Iy(x1, y2); f(x2, y1), f(x2, y2), Iy(x2, y1), Iy(x2, y2); Ix(x1, y1), Ix(x1, y2), Ixy(x1, y1), Ixy(x1, y2); Ix(x2, y1), Ix(x2, y2), Ixy(x2, y1), Ixy(x2, y2)];
+    third = [1, 0, -3, 2; 0, 0, 3, -2; 0, 1, -2, 1; 0, 0, -1, 1];
     % TODO: Converte?te matricile intermediare la double.
-
+    first = double(first);
+    second = double(second);
+    third = double(third);
     % TODO: Calculeaz? matricea final?.
-
+    A = first * second * third;
 endfunction
